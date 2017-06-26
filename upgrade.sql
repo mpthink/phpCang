044a079d3a668ffd928d8ac33e4946c15e8c9fe4
mpthink <mpthink@163.com>
2017-06-26 22:41:52 +0800

    update sql

--- upgrade.sql
+++ upgrade.sql
@@ -16,10 +16,11 @@ CREATE TABLE `twms_prod_deliver` (
 	PRIMARY KEY (`pddeliver_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
---
+--phase 2
 -- modify twms_outstore_main to store deliver id
 --
 alter table cang7.twms_outstore_main add `osm_deliver` int(2) DEFAULT NULL;
+
 
 --
 -- modify twms_product, 添加保质期
@@ -27,16 +28,20 @@ alter table cang7.twms_outstore_main add `osm_deliver` int(2) DEFAULT NULL;
 alter table cang7.twms_product add `prod_life` int(4) DEFAULT NULL;
 
 ---不合理表修改初始值
-alter table cang7.twms_instore_sub drop `iss_unit` varchar(10) DEFAULT NULL;
+alter table cang7.twms_instore_sub drop `iss_unit`;
 alter table cang7.twms_instore_sub modify `iss_quality` varchar(20) DEFAULT NULL;
 alter table cang7.twms_instore_sub drop `iss_insert_timestamp`;
 
 
-alter table cang7.twms_outstore_sub drop `oss_unit` varchar(10) DEFAULT NULL;
+alter table cang7.twms_outstore_sub drop `oss_unit`;
 alter table cang7.twms_outstore_sub modify `oss_quality` varchar(20) DEFAULT NULL;
 alter table cang7.twms_outstore_sub modify `oss_insert_order` int(11) DEFAULT NULL;
 
 
+--phase 3
+--添加生产日期
+alter table cang7.twms_instore_sub add iss_make_date timestamp NULL DEFAULT NULL;
+alter table cang7.twms_outstore_sub add oss_make_date timestamp NULL DEFAULT NULL;
 
 
 
